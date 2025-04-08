a685e195b4254221e92708c9103e261e888037f8
qap2102 <quanganhpham4869@gmail.com>
2025-04-08 18:08:07 +0700

    add file

--- DSA02006 DAY CON CO TONG BANG K.cpp
+++ DSA02006 DAY CON CO TONG BANG K.cpp
@@ -10,55 +10,57 @@ using namespace std;
 const int MOD = 1e9+7;
 const int N=1e6+5;
 
-
-int a[25];
-int n,x;
 vector<int> v;
 vector<vector<int>> vv;
-
+int a[1005];
+int n,k;
 bool kt = false;
 
-void Try1(int i, int sum){
+void Try(int i, int sum){
     for(int j=i;j<n;++j){
         v.push_back(a[j]);
-        if(sum+a[j]<=x) Try1(j, sum+a[j]);
+        if(sum+a[j]<=k) Try(j+1,sum+a[j]);
         v.pop_back();
     }
-    if(sum==x) {
+    if(sum==k) {
         kt=true;
         vv.push_back(v);
     }
+    
 }
-
 
 int main(){
     faster;
     int t;
     cin>>t;
     while(t--){
+        cin>>n>>k;
         v.clear();
         vv.clear();
-        cin>>n>>x;
-        for(int i=0;i<n;++i) cin>>a[i];
+        for(int i=0;i<n;++i){
+            cin>>a[i];
+        }
         sort(a,a+n);
         kt = false;
-        Try1(0,0);
+        Try(0,0);
         if(!kt) cout<<-1<<endl;
         else {
-            for(auto it:vv){
-                cout<<"["<<it[0];
-                for(int i=1;i<=it.size()-1;++i){
-                    cout<<" "<<it[i];
-                }
-                cout<<"]";
-            }
-            cout<<endl;
-        }
 
+        
+        for(auto it:vv){
+            cout<<"[";
+            for(int x:it){
+                if(x!=it[it.size()-1]){
+                    cout<<x<<" ";
+                }
+                else cout <<x;
+            }
+            cout<<"] ";
+        }
+        cout<<endl;
+        
 
 
     }
 }
-
-
-
+}
